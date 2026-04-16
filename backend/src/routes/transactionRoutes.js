bab112e0879bccbcd77e67a26c323ba3c8d1c37a
TuSHar-Sawale <125028683+TuSHar-Sawale@users.noreply.github.com>
2026-04-16 23:02:07 +0530

    Update transactionRoutes.js

--- backend/src/routes/transactionRoutes.js
+++ backend/src/routes/transactionRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { deposit, withdraw, transfer, getTransactions } = require("../controllers/transactionController");
+const { deposit, withdraw, transfer, getTransactions, searchUsers } = require("../controllers/transactionController");
 const { protect } = require("../middleware/authMiddleware");
 
+router.get("/search", protect, searchUsers); // <--- Add this
 router.post("/deposit", protect, deposit);
 router.post("/withdraw", protect, withdraw);
 router.post("/transfer", protect, transfer);
